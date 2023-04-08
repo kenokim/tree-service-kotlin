606bdbd5ad3267997ef6a4af6d8942715c896df4
kenokim <keno1413@gmail.com>
2023-04-08 18:41:41 +0900

    Implement entities

--- src/main/kotlin/com/example/treeservicekotlin/entity/Node.kt
+++ src/main/kotlin/com/example/treeservicekotlin/entity/Node.kt
@@ -1,14 +1,23 @@
 package com.example.treeservicekotlin.entity
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class Node(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val nodeId: Long,
-    val name: String
+
+    val name: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "node_id")
+    val parent: Node,
+
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+    val children: List<Node> = ArrayList(),
+
+    @OneToMany(mappedBy = "node", fetch = FetchType.LAZY)
+    val nutsList: List<Nuts> = ArrayList()
 ) {
+
 }
